b2f56f87b7e436752722fe21789b9efc9bd9fb4b
Elanur Bulbul <126582672+elanurbulbul@users.noreply.github.com>
2024-10-12 22:06:30 +0300

    updatedProject

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ıt is working by ela</Text>
+      <Text>ben ela</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
